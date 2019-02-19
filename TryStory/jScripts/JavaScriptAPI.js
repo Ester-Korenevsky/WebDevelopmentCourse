254a67ca5248c1f7e9efcc29907c49fbf328280c
Ester-Korenevsky <esterka94@gmail.com>
2019-02-19 22:47:07 +0200

    Add files via upload

---
+++ TryStory/jScripts/JavaScriptAPI.js
@@ -0,0 +1,40 @@
+﻿//מפה
+
+var myLocation = new google.maps.LatLng(32.0511351, 34.8372129);
+
+function initialize() {
+    var mapOptions = {
+        center: myLocation,
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+
+    var map = new google.maps.Map(document.getElementById("myGoogleMap"), mapOptions);
+    var myIcon = 'images/marker.png';
+
+    var marker = new google.maps.Marker({
+        position: myLocation,
+        animation: google.maps.Animation.DROP,
+        icon: myIcon
+    });
+    marker.setMap(map);
+
+
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "עמותת עמדא - הקו החם *8889"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+
+
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
