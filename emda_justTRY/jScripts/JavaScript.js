8f4dddcaf1ea7b65f97d1d4387f4a801fb50833e
Ester-Korenevsky <esterka94@gmail.com>
2019-06-17 19:47:25 +0300

    Add files via upload

---
+++ emda_justTRY/jScripts/JavaScript.js
@@ -0,0 +1,26 @@
+﻿
+$(document).ready(function () {
+
+  
+    //בהתאם לגודל המסך FOOTER בדיקה למיקום ה 
+    if ($("body").height() < $(window).height()) {
+        $('#myfooter').css('position', 'absolute');
+        $('#myfooter').css('bottom', 0);
+        console.log("הטקסט" + $("body").height());
+        console.log("המסך" + $(window).height());
+
+    }
+    $(window).resize(function () {
+        $('#myfooter').css('position', 'relative');
+        $('#myfooter').css('bottom', 'auto');
+        if ($("body").height() < $(window).height()) {
+            $('#myfooter').css('position', 'absolute');
+            $('#myfooter').css('bottom', 0);
+            console.log("הטקסט" + $("body").height());
+            console.log("המסך" + $(window).height());
+
+        }
+    });
+   
+});
+
